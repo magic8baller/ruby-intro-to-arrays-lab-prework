1bca7deba94b91d7f148c88b3c37b8b76bd0e2f1
Leslie Turis <lturis@gmail.com>
2018-09-01 20:00:44 +0000

    automatically backed up by learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -2,6 +2,7 @@ def instantiate_new_array
   []
 end
 
+
 def array_with_two_elements
   ['element1', 'element2']
 end
@@ -19,4 +20,19 @@ end
 
 def last_element(array)
   array[-1]
+end
+
+
+def first_element_with_array_methods(array)
+  array.first
+end
+
+
+def last_element_with_array_methods(array)
+  array.last
+end
+
+
+def length_of_array(array)
+  array.length
 end
